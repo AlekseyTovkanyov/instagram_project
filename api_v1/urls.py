e8c8535c7f1e4e8a05285d169a3d0f6f7c439b9c
AlekseyTovkanyov <lex.kramm@gmail.com>
2025-08-29 17:14:47 +0600

    Реализация удаления и добавления лайков в api, добавлен файл JS

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
-from api_v1.views import PostViewSet, LogoutView
+from api_v1.views import PostViewSet, LogoutView, LikeToggleView
 
 app_name = 'api_v1'
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('login/', obtain_auth_token, name='api_token_auth'),
     path('logout/', LogoutView.as_view(), name='api_token_delete'),
+    path('posts/<int:pk>/<str:action>/', LikeToggleView.as_view(), name='like-toggle'),
 ]
