e269b5891f56415a481ee4ab8b156d42cbf2e71f
AlekseyTovkanyov <lex.kramm@gmail.com>
2025-08-29 19:27:19 +0600

    исправил ошибки

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
-from api_v1.views import PostViewSet, LogoutView
+from api_v1.views import PostViewSet, LogoutView, LikeToggleView
 
 app_name = 'api_v1'
 
